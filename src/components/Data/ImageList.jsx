fab158a6c1afadcef96b94b14e61b6e75708cdf7
Fred'Dev <fredchenot@gmail.com>
2023-08-30 17:09:49 +0200

    bug affichage photo'

--- src/components/Data/ImageList.jsx
+++ src/components/Data/ImageList.jsx
@@ -1,24 +1,12 @@
 const importAll = (requireContext) => requireContext.keys().map(requireContext);
 const images = importAll(require.context('./Images', false));
-console.log('*********', images);
-
+const imagesPlus = importAll(require.context('./Images/ImagesPlus', false));
+console.log(images);
+console.log(imagesPlus);
 const itemData = [
   {
     img: images[0],
-    title: 'Ginko Biloba',
-    author: 'FoliFlore',
-    description: 'Dernier représentant d\'une famille apparue il y a plus de 270 millions d\'années, l’arbre aux quarante écus Ginkgo biloba peut vivre plus de 1000 ans. C\'est une espèce dioïque : un individu est soit mâle soit femelle.',
-
-  },
-  {
-    img: images[1],
-    title: 'Ginko Biloba',
-    author: 'FoliFlore',
-    description: 'Dernier représentant d\'une famille apparue il y a plus de 270 millions d\'années, l’arbre aux quarante écus Ginkgo biloba peut vivre plus de 1000 ans. C\'est une espèce dioïque : un individu est soit mâle soit femelle.Dernier représentant d\'une famille apparue il y a plus de 270 millions d\'années, l’arbre aux quarante écus Ginkgo biloba peut vivre plus de 1000 ans. C\'est une espèce dioïque : un individu est soit mâle soit femelle.Dernier représentant d\'une famille apparue il y a plus de 270 millions d\'années, l’arbre aux quarante écus Ginkgo biloba peut vivre plus de 1000 ans. C\'est une espèce dioïque : un individu est soit mâle soit femelle.Dernier représentant d\'une famille apparue il y a plus de 270 millions d\'années, l’arbre aux quarante écus Ginkgo biloba peut vivre plus de 1000 ans. C\'est une espèce dioïque : un individu est soit mâle soit femelle.Dernier représentant d\'une famille apparue il y a plus de 270 millions d\'années, l’arbre aux quarante écus Ginkgo biloba peut vivre plus de 1000 ans. C\'est une espèce dioïque : un individu est soit mâle soit femelle.',
-
-  },
-  {
-    img: images[2],
+    imgPlus: imagesPlus[0],
     title: 'Ginko Biloba',
     author: 'FoliFlore',
     description: 'Dernier représentant d\'une famille apparue il y a plus de 270 millions d\'années, l’arbre aux quarante écus Ginkgo biloba peut vivre plus de 1000 ans. C\'est une espèce dioïque : un individu est soit mâle soit femelle.',
@@ -26,15 +14,12 @@ const itemData = [
   },
   {
     img: images[3],
-    title: 'Ginko Biloba',
+    imgPlus: imagesPlus[3],
+    title: 'Asiminier',
     author: 'FoliFlore',
-    description: 'Dernier représentant d\'une famille apparue il y a plus de 270 millions d\'années, l’arbre aux quarante écus Ginkgo biloba peut vivre plus de 1000 ans. C\'est une espèce dioïque : un individu est soit mâle soit femelle.',
-
-  },
-  {
-    img: images[3],
-    title: 'Asiminier Triloba',
     description: 'Ce petit arbre originaire d’Amérique du Nord de la famille des annonacées développe un fruit de couleur vert jaune à maturité qui est dégusté cru, en crème glacée, sorbet, boisson fermentée ou pâtisserie. Le goût rappelle à la fois l’ananas et la banane à la texture fondante.',
+
   },
+
 ];
 export default itemData;
--- src/components/ImageList/ImageList.jsx
+++ src/components/ImageList/ImageList.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Stack from '@mui/material/Stack';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -10,39 +11,85 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 import './imageList.scss'; // Assurez-vous d'importer votre fichier SCSS ici
 import itemData from '../Data/ImageList';
 
+// JSX modifié
 function TitlebarBelowImageList() {
+  const [expandedAccordion, setExpandedAccordion] = useState(null);
+
+  const handleAccordionClick = (index) => {
+    if (index === expandedAccordion) {
+      setExpandedAccordion(null);
+    }
+    else {
+      setExpandedAccordion(index);
+    }
+  };
+
   return (
     <div className="Image-List">
       <ImageList className="image-list-container">
-        {itemData.map((item) => (
-          <ImageListItem key={item.img} className="custom-image-list-item">
-            <img
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              alt={item.title}
-              loading="lazy"
-              className="custom-image"
-            />
+        {itemData.map((item, index) => (
+          <ImageListItem
+            key={item.img}
+            className={`custom-image-list-item ${
+              index === expandedAccordion ? 'expanded' : ''
+            }`}
+          >
+            <div className="image-container">
+              <img
+                src={`${item.img}?w=248&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+                className="custom-image"
+              />
+            </div>
             <ImageListItemBar
               position="below"
               subtitle={(
                 <span>
                   <Stack spacing={2} direction="column" justifyContent="center" />
-                  <Accordion className="custom-accordion">
-                    <AccordionSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel1a-content"
-                      id="panel1a-header"
-                      className="accordion-summary"
+                  <div
+                    className={`custom-accordion ${
+                      index === expandedAccordion ? 'expanded' : ''
+                    }`}
+                  >
+                    <Accordion
+                      className="custom-accordion-content"
                     >
-                      <Typography><strong>{item.title}</strong></Typography>
-                    </AccordionSummary>
-                    <AccordionDetails className="accordion-details">
-                      <Typography>
-                        {item.description}
-                      </Typography>
-                    </AccordionDetails>
-                  </Accordion>
+                      <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                        onClick={() => handleAccordionClick(index)}
+                        className={`accordion-summary ${
+                          expandedAccordion === index ? 'Mui-expanded' : ''
+                        }`}
+                      >
+                        <Typography><strong>{item.title}</strong></Typography>
+                      </AccordionSummary>
+                      <AccordionDetails className="accordion-details">
+                        <Typography>
+                          {item.description}
+                          <img
+                            src={`${item.img}?w=248&fit=crop&auto=format`}
+                            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                            alt={item.title}
+                            loading="lazy"
+                            className="custom-image"
+                          />
+                          {/* {itemData.map((item) => (
+                            <img
+                              src={`${item.img}?w=248&fit=crop&auto=format`}
+                              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                              alt={item.title}
+                              loading="lazy"
+                              className="custom-image"
+                            />
+                          ))} */}
+                        </Typography>
+                      </AccordionDetails>
+                    </Accordion>
+                  </div>
                 </span>
               )}
             />
